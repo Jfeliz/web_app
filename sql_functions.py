69cb547a4ee449cc1d784ab86cea595502bbdb9d
Mark Moralls <mmoralls0@gmail.com>
2022-06-20 22:23:12 -0400

    Update sql_functions.py
    Updated. [6/20/22]

--- sql_functions.py
+++ sql_functions.py
@@ -2,10 +2,15 @@
 
 #load libraries
 import streamlit as st
-import os
 import pandas_gbq
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/mark/Documents/Data_Projects/web-app/web-app-341703-626adddc7d5a.json'
+#read credentails for BigQuery access
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 project_id = 'web-app-341703'
 
 def app():
@@ -20,8 +25,8 @@ def app():
     
     with col1:
         with st.form(key='query1', clear_on_submit = True):
-                st.write("This query selects the maximum prediceted readmission rate for all providers. [MAX]")
-                st.code("SELECT MAX(predicted_readmission_rate) AS Max_Readmission_Rate\nFROM hosp_info.readmission_reduction")
+                st.write("This query selects the maximum predicted readmission rate for all providers. [MAX]")
+                st.code("SELECT ROUND(MAX(predicted_readmission_rate), 1) AS Max_Readmission_Rate\nFROM hosp_info.readmission_reduction")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -42,8 +47,8 @@ def app():
     
     with col1:
         with st.form(key='query2', clear_on_submit = True):
-                st.write("This query counts the nuber of hospitals in California. [COUNT]")
-                st.code("SELECT COUNT(*) AS Number_of_Hospitals_in_Calif\nFROM hosp_info.hospital_general_information\nWHERE state = 'CA' [COUNT]")
+                st.write("This query counts the nubmer of hospitals in California. [COUNT]")
+                st.code("SELECT hospital_name, COUNT(*) AS Number_of_Hospitals_in_Calif\nFROM hosp_info.hospital_general_information\nWHERE state = 'CA'\nGROUP BY hospital_name")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -64,7 +69,7 @@ def app():
     
     with col1:
         with st.form(key='query3', clear_on_submit = True):
-                st.write("This query returns the length of characters in the hospital name [LENGTH]")
+                st.write("This query returns the length of characters in the hospital name. [LENGTH]")
                 st.code("SELECT hospital_name, LENGTH(hospital_name) AS Number_of_Characters\nFROM hosp_info.hospital_general_information\nLIMIT 10")
                 submit_code = st.form_submit_button("Execute") 
             
@@ -86,8 +91,8 @@ def app():
     
     with col1:
         with st.form(key='query4', clear_on_submit = True):
-                st.write("Returns the minimum uninsured rate. [MIN]")
-                st.code("SELECT MIN(disabled_medicare_rate) AS Min_Disabled_Medicare_Rate\nFROM hosp_info.measures_of_birth_and_death")
+                st.write("Returns the minimum life expectancy in the state of Alabama. [MIN]")
+                st.code("SELECT ROUND(MIN(avg_life_expect), 1) AS Min_Avg_Life_Expectancy\nFROM hosp_info.measures_of_birth_and_death\nWHERE state_name = 'Alabama'")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -96,7 +101,6 @@ def app():
                 contents = f.read()
                 df = pandas_gbq.read_gbq(contents, project_id)
                 st.table(df)
-                #st.write(df)
 
     with col2:
             # this line is a shortcut to clicking the hamburger menu to refresh
@@ -109,8 +113,8 @@ def app():
     
     with col1:
         with st.form(key='query5', clear_on_submit = True):
-                st.write("<Returns sum of all deaths from all hospitals [SUM]")
-                st.code("SELECT SUM(total_deaths) AS Sum_of_Deaths_all_Hospitals \nFROM hosp_info.measures_of_birth_and_death")
+                st.write("Returns sum of discharges from all hospitals [SUM]")
+                st.code("SELECT SUM(number_of_discharges) AS Sum_of_Deaths_all_Hospitals \nFROM hosp_info.readmission_reduction")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -131,8 +135,8 @@ def app():
     
     with col1:
         with st.form(key='query6', clear_on_submit = True):
-                st.write("Retunrs the sum of the primary care physician rate and then rounds it. [ROUND]")
-                st.code("SELECT ROUND(SUM(prim_care_phys_rate),1) as Primary_Care_Physician_Rate\nFROM hosp_info.measures_of_birth_and_death")
+                st.write("This query rounds the infant mortality rate for the the state of Alabama. [ROUND]")
+                st.code("SELECT ROUND(AVG(infant_mortality), 1) AS Infant_Mortality \nFROM hosp_info.measures_of_birth_and_death\nWHERE state_name = 'Alabama'")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -149,12 +153,12 @@ def app():
 #------Query7------
     with st.container():
 
-        col1, col2 = st.columns([5,1])
+        col1, col2 = st.columns([3,1])
     
     with col1:
         with st.form(key='query7', clear_on_submit = True):
                 st.write("Returns the number of providers in each city and state, then groups by city and state. [GROUP BY] ")
-                st.code("SELECT COUNT(provnum) provider_name, city, state\nFROM hosp_info.nursing_home_provider_info\nGROUP BY city, state\nLIMIT 10")
+                st.code("SELECT COUNT (DISTINCT(provider_name) AS Number_of_Providers, city, state\nFROM hosp_info.nursing_home_provider_info\nGROUP BY city, state\nLIMIT 25")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -176,7 +180,7 @@ def app():
     with col1:
         with st.form(key='query8', clear_on_submit = True):
                 st.write("Returns the average number of readmissions and rounds it. [AVG]")
-                st.code("SELECT AVG(number_of_readmissions) as Avg_Number_of_Readmissions\nFROM hosp_info.readmission_reduction")
+                st.code("SELECT ROUND(AVG(number_of_readmissions), 2) as Avg_Number_of_Readmissions\nFROM hosp_info.readmission_reduction")
                 submit_code = st.form_submit_button("Execute") 
             
     if submit_code:
@@ -197,10 +201,9 @@ def app():
     
     with col1:
         with st.form(key='query9', clear_on_submit = True):
-                st.write("Returns all hospitals by satate name with FIPS code over 10 [HAVING]")
-                st.code("SELECT COUNT(provider_id), state_name\nFROM hosp_info.measures_of_birth_and_death\nGROUP BY state_name\nHAVING COUNT(state_fips_code) > 10 ")
-                submit_code = st.form_submit_button("Execute") 
-            
+                st.write("Returns all hospitals by state name with FIPS code over 10 [HAVING]")
+                st.code("SELECT COUNT(infant_mortality), state_name\nFROM hosp_info.measures_of_birth_and_death\nGROUP BY state_name\nHAVING COUNT(infant_mortality) > 5\nORDER BY state_name;")
+                submit_code = st.form_submit_button("Execute")
     if submit_code:
 
         with open('queries/functions//HAVING.sql') as f:
