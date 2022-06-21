d4b96cb0eddd37bf7c590ea6637da06c3d6fe271
Mark Moralls <mmoralls0@gmail.com>
2022-06-20 22:17:58 -0400

    Update references.py
    Add subheaders, link to github repository. [6/20/22]

--- references.py
+++ references.py
@@ -4,8 +4,11 @@ def app():
     st.subheader('References')
     st.write('Data Analytics in Healthcare Research; Tools and Strategies; David T. Marc, MBS, CHDA | Ryan H. Sandefer, MA, CPHIT')
     st.write("\n")
+    st.subheader('Training')
     st.write("'Learning SQL Programming' from LinkedIn Learning")
     st.write("\n")
-    st.write("Link to Github repository")
+    st.subheader('Other')
+    link = '[Link](https://github.com/mmoralls/web_app) to my Github repository'
+    st.markdown(link,unsafe_allow_html=True)
 
     #include the author of the main app
