22688df82f6051ebcb8ff26eb78962b184947657
Mark Moralls <mmoralls0@gmail.com>
2022-06-24 23:44:39 -0400

    Update main-app.py

--- main-app.py
+++ main-app.py
@@ -6,7 +6,7 @@ import streamlit as st
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
-SCOPE = "https://www.googleapis.com/auth/userinfo.email"
+SCOPE = "https://oauth2.googleapis.com/token"
 
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"], scopes=[SCOPE]
