840caad8281a95a7eae6c0d8d1b9668380eba5c3
Mark Moralls <mmoralls0@gmail.com>
2022-06-27 09:59:00 -0400

    Update main-app.py

--- main-app.py
+++ main-app.py
@@ -7,7 +7,7 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
+    st.secrets["gcp_service_account"], scopes=["https://www.googleapis.com/auth/cloud-platform.read-only"]
 )
 client = bigquery.Client(credentials=credentials)
 
