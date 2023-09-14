a2dc4b56b8c220bed375720a6c820cde3a8e53a5
Mark Moralls <mmoralls0@gmail.com>
2023-09-14 17:40:18 -0400

    Update DISTINCT.sql

--- queries/basic/DISTINCT.sql
+++ queries/basic/DISTINCT.sql
@@ -1,3 +1,2 @@
-SELECT DISTINCT(county_name), hospital_name
-FROM hosp_info.hospital_general_information
-WHERE county_name = 'SAN FRANCISCO'
+SELECT DISTINCT(measure_name), hospital_name 
+FROM hosp_info.hospital_associated_infection
